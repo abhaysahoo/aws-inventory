f3a8a9d078bfde908d9bd01c8f831f157ae6873f
Abhay Sahoo <abhayshaw19999@gmail.com>
2024-09-26 15:12:22 +0530

    2nd commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,9 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 
+/* IMPORT ROUTES */
+import dashboardRoutes from "./routes/dashboardRoutes";
+
 /* CONFIGURATIONS */
 dotenv.config();
 const app = express();
@@ -17,9 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 /* ROUTES */
-app.get("/hello", (req, res) => {
-    res.send("hello world");
-});
+app.use("/dashboard", dashboardRoutes);
 
 /* SERVER */
 const port = process.env.PORT || 3001;
